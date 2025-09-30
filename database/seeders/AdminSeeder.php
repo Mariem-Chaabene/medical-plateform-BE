1d20e7ec19d739bacc9cdf6b262a885091d43657
mariem <chabenemariem@gmail.Com>
2025-09-30 23:38:44 +0100

    Ajout AdminSeeder pour la 1ere creation d'un admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin',
+                'surname' => 'Principal',
+                'password' => Hash::make('Password123!'),
+                'sexe' => 'homme',
+                'telephone' => '0000000000'
+            ]
+        );
+
+        $admin->assignRole('admin');
+
+        $this->command->info("✅ Premier admin créé !");
+    }
+}
